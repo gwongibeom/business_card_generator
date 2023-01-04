519c2c45800e79254a8aae16e9bcc3e967a23a5c
gwongibeom <k201gun@kakao.com>
2023-01-04 11:29:19 +0900

    first test export

--- 1_1랜덤_이미지_만들기_이미지텐서.py
+++ 1_1랜덤_이미지_만들기_이미지텐서.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+np.random.randint(100,400)
+
+
+
+
+
+
+"""#언팩킹 (변수 여러개에 한번에 매칭하기)"""
+
+x,y = [1,2]
+
+
+x,y = np.random.randint(100,400,size=2)
+
 
 
 
@@ -32,13 +46,12 @@ import numpy as np
 
 이미지 텐서
 """
+image_tensor = np.random.randint(256,size=(x,y,3))
+
+image_tensor.shape
 
 
-
-
-
-
-
+image_tensor.astype('uint8')
 
 
 
@@ -58,14 +71,14 @@ import numpy as np
 
 
 """이미지 생성하기"""
+from PIL import Image
 
-
-
+result = Image.fromarray(image_tensor,'RGB')
 
 
 """이미지 저장하기"""
-
+result.save("test.jpg")
 
 
 """이미지 닫기"""
-
+result.close
